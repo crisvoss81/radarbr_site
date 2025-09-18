8f431b507569271d82a34cf71b86eeea1a8e9c27
crisvoss81 <crisvossrodrigues@gmail.com>
2025-09-18 09:57:10 -0300

    fix: sitemaps module and Django sitemaps app

--- siteapp/sitemaps.py
+++ siteapp/sitemaps.py
@@ -3,24 +3,36 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from .models import Post, Category
 
+
 class PostSitemap(Sitemap):
     changefreq = "hourly"
-    priority = 0.9
+    priority = 0.8
+
     def items(self):
+        # só posts publicados
         return Post.objects.filter(is_published=True)
+
     def lastmod(self, obj):
-        return obj.published_at
+        # usa published_at ou updated_at se existir
+        return getattr(obj, "published_at", None) or getattr(obj, "updated_at", None)
+
     def location(self, obj):
         return obj.get_absolute_url()
 
+
 class CategorySitemap(Sitemap):
     changefreq = "daily"
-    priority = 0.6
+    priority = 0.5
+
     def items(self):
         return Category.objects.all()
+
     def location(self, obj):
-        # ajuste se seu model já tiver get_absolute_url
-        try:
-            return obj.get_absolute_url()
-        except AttributeError:
-            return reverse("category_detail", args=[obj.slug])
+        return reverse("category_detail", args=[obj.slug])
+
+
+# ESTE é o dict que o core/urls.py importa
+sitemaps = {
+    "posts": PostSitemap,
+    "categories": CategorySitemap,
+}
