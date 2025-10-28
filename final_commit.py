28f90aa72851748ab15d16e51364c22e159d0328
crisvoss81 <crisvossrodrigues@gmail.com>
2025-10-28 10:02:29 -0300

    Header: link do ícone do Facebook vai para a página oficial salva no admin (usar config.facebook_url quando disponível).

--- final_commit.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-
-subprocess.run(['git', 'add', '-A'])
-subprocess.run(['git', 'commit', '-m', 'Adicionar documentação sobre signals do Facebook'])
-subprocess.run(['git', 'push', 'origin', 'main'])
-
-print("✅ Enviado!")
-
